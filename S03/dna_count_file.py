1f019df30b14cbad086bf9037bd364277fe36e72
evamarti <e.martinezc.2023@alumnos.urjc>
2024-01-31 18:20:29 +0100

    First but not last commit

--- S03/dna_count_file.py
+++ S03/dna_count_file.py
@@ -1,17 +1,18 @@
 file = open("dna.txt", "r")
 dict = {}
 len = 0
-for e in file:
-    seq = e.replace("\n", "")
-    for i in seq:
-        if not i in dict:
-            dict[i] = 1
-        dict[i] += 1
+with open("dna.txt", "r") as file:
+    for e in file:
+        seq = e.replace("\n", "")
+        for i in seq:
+            if not i in dict:
+                dict[i] = 1
+            dict[i] += 1
 
 length = int(dict["A"]) + int(dict["T"]) + int(dict["G"]) + int(dict["C"])
 print(dict)
 print("The length is", length)
-file.close()
+
 
 
 
